a9e73d0e858ba671e13aa54e5376ec9f782ee156
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-08-12 00:21:21 +0200

    add schema files to bq calls

--- bigquery/sync.sh
+++ bigquery/sync.sh
@@ -28,14 +28,19 @@ function help {
 
 function main {
     date_string=`date +"%Y-%m-%d"`
+    home_dir="/home/gostats/bigquery/"
 
     trending_repos="${data_dir}/github_trending_repos-${date_string}.json"
     echo "Syncing ${trending_repos}..."
-    /home/gostats/google-cloud-sdk/bin/bq load --source_format=NEWLINE_DELIMITED_JSON github.trending ${trending_repos}
+    /home/gostats/google-cloud-sdk/bin/bq load --source_format=NEWLINE_DELIMITED_JSON github.trending ${trending_repos} ${home_dir}github_schema.json
     
+    starred_repos="${data_dir}/github_most_starred-${date_string}.json"
+    echo "Syncing ${starred_repos}..."
+    /home/gostats/google-cloud-sdk/bin/bq load --source_format=NEWLINE_DELIMITED_JSON godoc.packages ${starred_repos} ${home_dir}github_schema.json
+
     godoc_packages="${data_dir}/godoc_packages-${date_string}.json"
     echo "Syncing ${godoc_packages}..."
-    /home/gostats/google-cloud-sdk/bin/bq load --source_format=NEWLINE_DELIMITED_JSON godoc.packages ${godoc_packages}
+    /home/gostats/google-cloud-sdk/bin/bq load --source_format=NEWLINE_DELIMITED_JSON godoc.packages ${godoc_packages} ${home_dir}godoc_schema.json
 
     echo "Done"
 }
